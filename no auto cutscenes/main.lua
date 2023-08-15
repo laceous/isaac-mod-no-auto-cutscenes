3a1524c89268db319d6d6a0ef78b7f85eb8da608
laceous <laceous@gmail.com>
2023-08-14 21:23:40 -0700

    edge case

--- no auto cutscenes/main.lua
+++ no auto cutscenes/main.lua
@@ -229,7 +229,8 @@ function mod:spawnMegaSatanDoorExit()
   local room = level:GetCurrentRoom()
   
   -- this goes to DOWN0 because it's the only available door slot
-  if room:TrySpawnBlueWombDoor(false, true, true) then -- TrySpawnBossRushDoor
+  -- DOWN0 can sometimes be taken in challenges
+  if room:GetDoor(DoorSlot.DOWN0) == nil and room:TrySpawnBlueWombDoor(false, true, true) then -- TrySpawnBossRushDoor
     local door = room:GetDoor(DoorSlot.DOWN0)
     if door then
       local sprite = door:GetSprite()
